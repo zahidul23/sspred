ae0b56aad512ed65b26c256e1020505ad7f6c1a3
wownice <whuang4830@bths.edu>
2019-11-08 23:11:11 -0500

    fixed validation for site selection, added fields for known sequences

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import TextAreaField, SubmitField, StringField, BooleanField, valid
 from wtforms.widgets import TextArea
 
 import re
-	
+
 class SubmissionForm(FlaskForm):
 	
 	seqtext = TextAreaField('Sequence', [
@@ -21,5 +21,26 @@ class SubmissionForm(FlaskForm):
 	Yaspin = BooleanField('YASPIN', [validators.Optional()], default="checked")
 	SSPro = BooleanField('SSPRO', [validators.Optional()], default="checked")
 	
+	structureId = StringField('Structure Id:', render_kw={'style':'width:80px'})
+	chainId = StringField('Chain Id:', render_kw={'style':'width:50px'})
+	
 	submitbtn = SubmitField('Submit')
-
+	
+	#Override default validate
+	def validate(self):
+		if not FlaskForm.validate(self):
+			return False
+		
+		validated = True
+		
+		#Check if at least one site selected
+		if not self.JPred.data and not self.PSI.data and not self.PSS.data and not self.RaptorX.data and not self.Sable.data and not self.Yaspin.data and not self.SSPro.data:
+			self.JPred.errors.append('At least one site must be selected.')
+			validated = False
+		
+		#If at least one field is filled, the other must be filled as well
+		if (self.structureId.data and not self.chainId.data) or (self.chainId.data and not self.structureId.data):
+			self.structureId.errors.append('A chain id must be provided with a structure id.')
+			validated = False
+		
+		return validated
